d7de78e2281002e3f5bc60ac2499fb8724987825
Tom Weatherhead <tomw@2hrd4u.org>
2019-11-19 11:54:00 -0700

    Updated the Chrome version number in the Protractor config file

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -14,7 +14,7 @@ exports.config = {
 	],
 	capabilities: {
 		'browserName': 'chrome',
-		chromeDriver: './node_modules/protractor/node_modules/webdriver-manager/selenium/chromedriver_78.0.3904.70',
+		chromeDriver: './node_modules/protractor/node_modules/webdriver-manager/selenium/chromedriver_78.0.3904.108',
 		chromeOptions: {
 			args: ['--headless', '--disable-gpu', '--window-size=1024,768', '--no-sandbox']
 		}
