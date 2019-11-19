59908ab671210267d0502004de0873db77b73197
Tom Weatherhead <tomw@2hrd4u.org>
2019-11-19 11:39:41 -0700

    e2e testing: Chrome must run headless

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -8,25 +8,29 @@ const { SpecReporter } = require('jasmine-spec-reporter');
  * @type { import("protractor").Config }
  */
 exports.config = {
-  allScriptsTimeout: 11000,
-  specs: [
-    './src/**/*.e2e-spec.ts'
-  ],
-  capabilities: {
-    browserName: 'chrome'
-  },
-  directConnect: true,
-  baseUrl: 'http://localhost:4200/',
-  framework: 'jasmine',
-  jasmineNodeOpts: {
-    showColors: true,
-    defaultTimeoutInterval: 30000,
-    print: function() {}
-  },
-  onPrepare() {
-    require('ts-node').register({
-      project: require('path').join(__dirname, './tsconfig.json')
-    });
-    jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
-  }
+	allScriptsTimeout: 11000,
+	specs: [
+		'./src/**/*.e2e-spec.ts'
+	],
+	capabilities: {
+		'browserName': 'chrome',
+		chromeDriver: './node_modules/protractor/node_modules/webdriver-manager/selenium/chromedriver_78.0.3904.70',
+		chromeOptions: {
+			args: ['--headless', '--disable-gpu', '--window-size=1024,768', '--no-sandbox']
+		}
+	},
+	directConnect: true,
+	baseUrl: 'http://localhost:4200/',
+	framework: 'jasmine',
+	jasmineNodeOpts: {
+		showColors: true,
+		defaultTimeoutInterval: 30000,
+		print: function() {}
+	},
+	onPrepare() {
+		require('ts-node').register({
+			project: require('path').join(__dirname, './tsconfig.json')
+		});
+		jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+	}
 };
